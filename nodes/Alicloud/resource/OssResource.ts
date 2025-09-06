02982e917d3ccdbd97ff336230d146c3be84e310
crazyyanchao <yanchaoma@foxmail.com>
2025-09-06 13:21:31 +0800

    Update OssResource to set display name to an empty string and add description and action properties

--- nodes/Alicloud/resource/OssResource.ts
+++ nodes/Alicloud/resource/OssResource.ts
@@ -1,8 +1,14 @@
 import { INodePropertyOptions } from 'n8n-workflow';
 
+// const OssResource: INodePropertyOptions = {
+// 	name: 'Object Storage Service (OSS)',
+// 	value: 'oss',
+// };
 const OssResource: INodePropertyOptions = {
-	name: 'Object Storage Service (OSS)',
+	name: '',
 	value: 'oss',
+	description: '阿里云对象存储服务',
+	action: 'open'
 };
 
 export default OssResource;
